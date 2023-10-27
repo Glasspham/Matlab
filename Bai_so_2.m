0b7e0628885334d20b8d30face057a0d48d645b8
Glasspham <95564442+Glasspham@users.noreply.github.com>
2023-10-27 23:07:45 +0700

    Nội dung câu hỏi
    Thực hiện các phép toán cho ma trận A chỉ bằng một dòng lệnh như sau:
    a. Cho ma trận A= reshape(1:36,6,6).
    b. Trích xuất phần tử hàng 3, cột 5.
    c. Trích xuất phần tử (1,4) ; (2,4) ; (3,4).
    d. Trích xuất phần tử ở hàng 4 cột 1 3 lần.
    e. Trích xuất 4 phần tử có chỉ số như sau: (2,3); (2,1) ; (5,3); (5,1).
    f. Trích xuất cột thứ 4.
    g. Trích xuất hàng thứ 4.
    h. Chuyển ma trận về thành vector cột.
    í. Trích xuất các phần tử ở hàng cuối cùng, cột 3.
    j. Trích xuất ma trận vuông 2×2 trên đường chéo chính có phần tử ở hàng và cột cuối cùng.
    k. Trích xuất 4 phần tử cuối cùng ở cột đầu tiên.
    L. Trích xuất tất cả phần tử theo thứ tự hàng ngược.
    m. Trích xuất ma trận đường chéo chính.

--- Bai_so_2.m
+++ Bai_so_2.m
@@ -1,18 +1,3 @@
-Thực hiện các phép toán cho ma trận A chỉ bằng một dòng lệnh như sau:
-a. Cho ma trận A= reshape(1:36,6,6).
-b. Trích xuất phần tử hàng 3, cột 5.
-c. Trích xuất phần tử (1,4) ; (2,4) ; (3,4).
-d. Trích xuất phần tử ở hàng 4 cột 1 3 lần.
-e. Trích xuất 4 phần tử có chỉ số như sau: (2,3); (2,1) ; (5,3); (5,1).
-f. Trích xuất cột thứ 4.
-g. Trích xuất hàng thứ 4.
-h. Chuyển ma trận về thành vector cột.
-í. Trích xuất các phần tử ở hàng cuối cùng, cột 3.
-j. Trích xuất ma trận vuông 2×2 trên đường chéo chính có phần tử ở hàng và cột cuối cùng.
-k. Trích xuất 4 phần tử cuối cùng ở cột đầu tiên.
-L. Trích xuất tất cả phần tử theo thứ tự hàng ngược.
-m. Trích xuất ma trận đường chéo chính.
-              Giải
 %a.
 A = reshape(1:36,6,6)
 %b.
